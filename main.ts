308919430bd6b03a859cb0698af141774d7176ae
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-09-14 05:07:14 -0400

    Learnding typescript

--- main.ts
+++ main.ts
@@ -4,5 +4,16 @@ import { print } from "./source/helpers"
 
 // I have no idea why I did this but I love it.
 // Spells banana
-print(((('b' + "a") + ([['\b'] + 'b' + 'b\n'] ["ba"] + 'ba' + + + [""] - "a" + 'a') * 1 || -1) + "a").toLocaleLowerCase() + "!");
+// print(((('b' + "a") + ([['\b'] + 'b' + 'b\n'] ["ba"] + 'ba' + + + [""] - "a" + 'a') * 1 || -1) + "a").toLocaleLowerCase() + "!");
 
+// print("hi")
+
+var test = [];
+
+for (let i = 0; i < 10; i++) {
+  test.push(i)
+}
+
+test.forEach((i)=>(print(i)))
+// print(test)
+// print("anything")
