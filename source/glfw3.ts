77e04adc41b81b767e71a5f728a479d20b25449b
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2023-09-16 03:10:39 -0400

    Didn't know you can do this

--- source/glfw3.ts
+++ source/glfw3.ts
@@ -36,195 +36,195 @@ function toBuffer(input: string) {
   return Buffer.from(input + '\0')
 }
 
-const GLFW_FOCUSED                  = 0x00020001
-const GLFW_ICONIFIED                = 0x00020002
-const GLFW_RESIZABLE                = 0x00020003
-const GLFW_VISIBLE                  = 0x00020004
-const GLFW_DECORATED                = 0x00020005
-const GLFW_AUTO_ICONIFY             = 0x00020006
-const GLFW_FLOATING                 = 0x00020007
-const GLFW_MAXIMIZED                = 0x00020008
-const GLFW_CENTER_CURSOR            = 0x00020009
-const GLFW_TRANSPARENT_FRAMEBUFFER  = 0x0002000A
-const GLFW_HOVERED                  = 0x0002000B
-const GLFW_FOCUS_ON_SHOW            = 0x0002000C
-const GLFW_RED_BITS                 = 0x00021001
-const GLFW_GREEN_BITS               = 0x00021002
-const GLFW_BLUE_BITS                = 0x00021003
-const GLFW_ALPHA_BITS               = 0x00021004
-const GLFW_DEPTH_BITS               = 0x00021005
-const GLFW_STENCIL_BITS             = 0x00021006
-const GLFW_ACCUM_RED_BITS           = 0x00021007
-const GLFW_ACCUM_GREEN_BITS         = 0x00021008
-const GLFW_ACCUM_BLUE_BITS          = 0x00021009
-const GLFW_ACCUM_ALPHA_BITS         = 0x0002100A
-const GLFW_AUX_BUFFERS              = 0x0002100B
-const GLFW_STEREO                   = 0x0002100C
-const GLFW_SAMPLES                  = 0x0002100D
-const GLFW_SRGB_CAPABLE             = 0x0002100E
-const GLFW_REFRESH_RATE             = 0x0002100F
-const GLFW_DOUBLEBUFFER             = 0x00021010
-const GLFW_CLIENT_API               = 0x00022001
-const GLFW_CONTEXT_VERSION_MAJOR    = 0x00022002
-const GLFW_CONTEXT_VERSION_MINOR    = 0x00022003
-const GLFW_CONTEXT_REVISION         = 0x00022004
-const GLFW_CONTEXT_ROBUSTNESS       = 0x00022005
-const GLFW_OPENGL_FORWARD_COMPAT    = 0x00022006
-const GLFW_OPENGL_DEBUG_CONTEXT     = 0x00022007
-const GLFW_OPENGL_PROFILE           = 0x00022008
-const GLFW_CONTEXT_RELEASE_BEHAVIOR = 0x00022009
-const GLFW_CONTEXT_NO_ERROR         = 0x0002200A
-const GLFW_CONTEXT_CREATION_API     = 0x0002200B
-const GLFW_SCALE_TO_MONITOR         = 0x0002200C
-const GLFW_COCOA_RETINA_FRAMEBUFFER = 0x00023001
-const GLFW_COCOA_FRAME_NAME         = 0x00023002
-const GLFW_COCOA_GRAPHICS_SWITCHING = 0x00023003
-const GLFW_X11_CLASS_NAME           = 0x00024001
-const GLFW_X11_INSTANCE_NAME        = 0x00024002
+export const GLFW_FOCUSED                  = 0x00020001
+export const GLFW_ICONIFIED                = 0x00020002
+export const GLFW_RESIZABLE                = 0x00020003
+export const GLFW_VISIBLE                  = 0x00020004
+export const GLFW_DECORATED                = 0x00020005
+export const GLFW_AUTO_ICONIFY             = 0x00020006
+export const GLFW_FLOATING                 = 0x00020007
+export const GLFW_MAXIMIZED                = 0x00020008
+export const GLFW_CENTER_CURSOR            = 0x00020009
+export const GLFW_TRANSPARENT_FRAMEBUFFER  = 0x0002000A
+export const GLFW_HOVERED                  = 0x0002000B
+export const GLFW_FOCUS_ON_SHOW            = 0x0002000C
+export const GLFW_RED_BITS                 = 0x00021001
+export const GLFW_GREEN_BITS               = 0x00021002
+export const GLFW_BLUE_BITS                = 0x00021003
+export const GLFW_ALPHA_BITS               = 0x00021004
+export const GLFW_DEPTH_BITS               = 0x00021005
+export const GLFW_STENCIL_BITS             = 0x00021006
+export const GLFW_ACCUM_RED_BITS           = 0x00021007
+export const GLFW_ACCUM_GREEN_BITS         = 0x00021008
+export const GLFW_ACCUM_BLUE_BITS          = 0x00021009
+export const GLFW_ACCUM_ALPHA_BITS         = 0x0002100A
+export const GLFW_AUX_BUFFERS              = 0x0002100B
+export const GLFW_STEREO                   = 0x0002100C
+export const GLFW_SAMPLES                  = 0x0002100D
+export const GLFW_SRGB_CAPABLE             = 0x0002100E
+export const GLFW_REFRESH_RATE             = 0x0002100F
+export const GLFW_DOUBLEBUFFER             = 0x00021010
+export const GLFW_CLIENT_API               = 0x00022001
+export const GLFW_CONTEXT_VERSION_MAJOR    = 0x00022002
+export const GLFW_CONTEXT_VERSION_MINOR    = 0x00022003
+export const GLFW_CONTEXT_REVISION         = 0x00022004
+export const GLFW_CONTEXT_ROBUSTNESS       = 0x00022005
+export const GLFW_OPENGL_FORWARD_COMPAT    = 0x00022006
+export const GLFW_OPENGL_DEBUG_CONTEXT     = 0x00022007
+export const GLFW_OPENGL_PROFILE           = 0x00022008
+export const GLFW_CONTEXT_RELEASE_BEHAVIOR = 0x00022009
+export const GLFW_CONTEXT_NO_ERROR         = 0x0002200A
+export const GLFW_CONTEXT_CREATION_API     = 0x0002200B
+export const GLFW_SCALE_TO_MONITOR         = 0x0002200C
+export const GLFW_COCOA_RETINA_FRAMEBUFFER = 0x00023001
+export const GLFW_COCOA_FRAME_NAME         = 0x00023002
+export const GLFW_COCOA_GRAPHICS_SWITCHING = 0x00023003
+export const GLFW_X11_CLASS_NAME           = 0x00024001
+export const GLFW_X11_INSTANCE_NAME        = 0x00024002
 
 //* Error codes
-const GLFW_NO_ERROR            = 0
-const GLFW_NOT_INITIALIZED     = 0x00010001
-const GLFW_NO_CURRENT_CONTEXT  = 0x00010002
-const GLFW_INVALID_ENUM        = 0x00010003
-const GLFW_INVALID_VALUE       = 0x00010004
-const GLFW_OUT_OF_MEMORY       = 0x00010005
-const GLFW_API_UNAVAILABLE     = 0x00010006
-const GLFW_VERSION_UNAVAILABLE = 0x00010007
-const GLFW_PLATFORM_ERROR      = 0x00010008
-const GLFW_FORMAT_UNAVAILABLE  = 0x00010009
-const GLFW_NO_WINDOW_CONTEXT   = 0x0001000A
+export const GLFW_NO_ERROR            = 0
+export const GLFW_NOT_INITIALIZED     = 0x00010001
+export const GLFW_NO_CURRENT_CONTEXT  = 0x00010002
+export const GLFW_INVALID_ENUM        = 0x00010003
+export const GLFW_INVALID_VALUE       = 0x00010004
+export const GLFW_OUT_OF_MEMORY       = 0x00010005
+export const GLFW_API_UNAVAILABLE     = 0x00010006
+export const GLFW_VERSION_UNAVAILABLE = 0x00010007
+export const GLFW_PLATFORM_ERROR      = 0x00010008
+export const GLFW_FORMAT_UNAVAILABLE  = 0x00010009
+export const GLFW_NO_WINDOW_CONTEXT   = 0x0001000A
 
 //* Whatever this is https://www.glfw.org/docs/latest/group__init.html
-const GLFW_TRUE                  = 1
-const GLFW_FALSE                 = 0
-const GLFW_JOYSTICK_HAT_BUTTONS  = 0x00050001
-const GLFW_COCOA_CHDIR_RESOURCES = 0x00051001
-const GLFW_COCOA_MENUBAR         = 0x00051002
+export const GLFW_TRUE                  = 1
+export const GLFW_FALSE                 = 0
+export const GLFW_JOYSTICK_HAT_BUTTONS  = 0x00050001
+export const GLFW_COCOA_CHDIR_RESOURCES = 0x00051001
+export const GLFW_COCOA_MENUBAR         = 0x00051002
 
 //* https://www.glfw.org/docs/latest/group__keys.html
-const GLFW_KEY_UNKNOWN       = -1
-const GLFW_KEY_SPACE         = 32
-const GLFW_KEY_APOSTROPHE    = 39 /* ' */
-const GLFW_KEY_COMMA         = 44 /* , */
-const GLFW_KEY_MINUS         = 45 /* - */
-const GLFW_KEY_PERIOD        = 46 /* . */
-const GLFW_KEY_SLASH         = 47 /* / */
-const GLFW_KEY_0             = 48
-const GLFW_KEY_1             = 49
-const GLFW_KEY_2             = 50
-const GLFW_KEY_3             = 51
-const GLFW_KEY_4             = 52
-const GLFW_KEY_5             = 53
-const GLFW_KEY_6             = 54
-const GLFW_KEY_7             = 55
-const GLFW_KEY_8             = 56
-const GLFW_KEY_9             = 57
-const GLFW_KEY_SEMICOLON     = 59 /* ; */
-const GLFW_KEY_EQUAL         = 61 /* = */
-const GLFW_KEY_A             = 65
-const GLFW_KEY_B             = 66
-const GLFW_KEY_C             = 67
-const GLFW_KEY_D             = 68
-const GLFW_KEY_E             = 69
-const GLFW_KEY_F             = 70
-const GLFW_KEY_G             = 71
-const GLFW_KEY_H             = 72
-const GLFW_KEY_I             = 73
-const GLFW_KEY_J             = 74
-const GLFW_KEY_K             = 75
-const GLFW_KEY_L             = 76
-const GLFW_KEY_M             = 77
-const GLFW_KEY_N             = 78
-const GLFW_KEY_O             = 79
-const GLFW_KEY_P             = 80
-const GLFW_KEY_Q             = 81
-const GLFW_KEY_R             = 82
-const GLFW_KEY_S             = 83
-const GLFW_KEY_T             = 84
-const GLFW_KEY_U             = 85
-const GLFW_KEY_V             = 86
-const GLFW_KEY_W             = 87
-const GLFW_KEY_X             = 88
-const GLFW_KEY_Y             = 89
-const GLFW_KEY_Z             = 90
-const GLFW_KEY_LEFT_BRACKET  = 91 /* [ */
-const GLFW_KEY_BACKSLASH     = 92 /* \ */
-const GLFW_KEY_RIGHT_BRACKET = 93 /* ] */
-const GLFW_KEY_GRAVE_ACCENT  = 96 /* ` */
-const GLFW_KEY_WORLD_1       = 161 /* non-US #1 */
-const GLFW_KEY_WORLD_2       = 162 /* non-US #2 */
-const GLFW_KEY_ESCAPE        = 256
-const GLFW_KEY_ENTER         = 257
-const GLFW_KEY_TAB           = 258
-const GLFW_KEY_BACKSPACE     = 259
-const GLFW_KEY_INSERT        = 260
-const GLFW_KEY_DELETE        = 261
-const GLFW_KEY_RIGHT         = 262
-const GLFW_KEY_LEFT          = 263
-const GLFW_KEY_DOWN          = 264
-const GLFW_KEY_UP            = 265
-const GLFW_KEY_PAGE_UP       = 266
-const GLFW_KEY_PAGE_DOWN     = 267
-const GLFW_KEY_HOME          = 268
-const GLFW_KEY_END           = 269
-const GLFW_KEY_CAPS_LOCK     = 280
-const GLFW_KEY_SCROLL_LOCK   = 281
-const GLFW_KEY_NUM_LOCK      = 282
-const GLFW_KEY_PRINT_SCREEN  = 283
-const GLFW_KEY_PAUSE         = 284
-const GLFW_KEY_F1            = 290
-const GLFW_KEY_F2            = 291
-const GLFW_KEY_F3            = 292
-const GLFW_KEY_F4            = 293
-const GLFW_KEY_F5            = 294
-const GLFW_KEY_F6            = 295
-const GLFW_KEY_F7            = 296
-const GLFW_KEY_F8            = 297
-const GLFW_KEY_F9            = 298
-const GLFW_KEY_F10           = 299
-const GLFW_KEY_F11           = 300
-const GLFW_KEY_F12           = 301
-const GLFW_KEY_F13           = 302
-const GLFW_KEY_F14           = 303
-const GLFW_KEY_F15           = 304
-const GLFW_KEY_F16           = 305
-const GLFW_KEY_F17           = 306
-const GLFW_KEY_F18           = 307
-const GLFW_KEY_F19           = 308
-const GLFW_KEY_F20           = 309
-const GLFW_KEY_F21           = 310
-const GLFW_KEY_F22           = 311
-const GLFW_KEY_F23           = 312
-const GLFW_KEY_F24           = 313
-const GLFW_KEY_F25           = 314
-const GLFW_KEY_KP_0          = 320
-const GLFW_KEY_KP_1          = 321
-const GLFW_KEY_KP_2          = 322
-const GLFW_KEY_KP_3          = 323
-const GLFW_KEY_KP_4          = 324
-const GLFW_KEY_KP_5          = 325
-const GLFW_KEY_KP_6          = 326
-const GLFW_KEY_KP_7          = 327
-const GLFW_KEY_KP_8          = 328
-const GLFW_KEY_KP_9          = 329
-const GLFW_KEY_KP_DECIMAL    = 330
-const GLFW_KEY_KP_DIVIDE     = 331
-const GLFW_KEY_KP_MULTIPLY   = 332
-const GLFW_KEY_KP_SUBTRACT   = 333
-const GLFW_KEY_KP_ADD        = 334
-const GLFW_KEY_KP_ENTER      = 335
-const GLFW_KEY_KP_EQUAL      = 336
-const GLFW_KEY_LEFT_SHIFT    = 340
-const GLFW_KEY_LEFT_CONTROL  = 341
-const GLFW_KEY_LEFT_ALT      = 342
-const GLFW_KEY_LEFT_SUPER    = 343
-const GLFW_KEY_RIGHT_SHIFT   = 344
-const GLFW_KEY_RIGHT_CONTROL = 345
-const GLFW_KEY_RIGHT_ALT     = 346
-const GLFW_KEY_RIGHT_SUPER   = 347
-const GLFW_KEY_MENU          = 348
-const GLFW_KEY_LAST          = GLFW_KEY_MENU
+export const GLFW_KEY_UNKNOWN       = -1
+export const GLFW_KEY_SPACE         = 32
+export const GLFW_KEY_APOSTROPHE    = 39 /* ' */
+export const GLFW_KEY_COMMA         = 44 /* , */
+export const GLFW_KEY_MINUS         = 45 /* - */
+export const GLFW_KEY_PERIOD        = 46 /* . */
+export const GLFW_KEY_SLASH         = 47 /* / */
+export const GLFW_KEY_0             = 48
+export const GLFW_KEY_1             = 49
+export const GLFW_KEY_2             = 50
+export const GLFW_KEY_3             = 51
+export const GLFW_KEY_4             = 52
+export const GLFW_KEY_5             = 53
+export const GLFW_KEY_6             = 54
+export const GLFW_KEY_7             = 55
+export const GLFW_KEY_8             = 56
+export const GLFW_KEY_9             = 57
+export const GLFW_KEY_SEMICOLON     = 59 /* ; */
+export const GLFW_KEY_EQUAL         = 61 /* = */
+export const GLFW_KEY_A             = 65
+export const GLFW_KEY_B             = 66
+export const GLFW_KEY_C             = 67
+export const GLFW_KEY_D             = 68
+export const GLFW_KEY_E             = 69
+export const GLFW_KEY_F             = 70
+export const GLFW_KEY_G             = 71
+export const GLFW_KEY_H             = 72
+export const GLFW_KEY_I             = 73
+export const GLFW_KEY_J             = 74
+export const GLFW_KEY_K             = 75
+export const GLFW_KEY_L             = 76
+export const GLFW_KEY_M             = 77
+export const GLFW_KEY_N             = 78
+export const GLFW_KEY_O             = 79
+export const GLFW_KEY_P             = 80
+export const GLFW_KEY_Q             = 81
+export const GLFW_KEY_R             = 82
+export const GLFW_KEY_S             = 83
+export const GLFW_KEY_T             = 84
+export const GLFW_KEY_U             = 85
+export const GLFW_KEY_V             = 86
+export const GLFW_KEY_W             = 87
+export const GLFW_KEY_X             = 88
+export const GLFW_KEY_Y             = 89
+export const GLFW_KEY_Z             = 90
+export const GLFW_KEY_LEFT_BRACKET  = 91 /* [ */
+export const GLFW_KEY_BACKSLASH     = 92 /* \ */
+export const GLFW_KEY_RIGHT_BRACKET = 93 /* ] */
+export const GLFW_KEY_GRAVE_ACCENT  = 96 /* ` */
+export const GLFW_KEY_WORLD_1       = 161 /* non-US #1 */
+export const GLFW_KEY_WORLD_2       = 162 /* non-US #2 */
+export const GLFW_KEY_ESCAPE        = 256
+export const GLFW_KEY_ENTER         = 257
+export const GLFW_KEY_TAB           = 258
+export const GLFW_KEY_BACKSPACE     = 259
+export const GLFW_KEY_INSERT        = 260
+export const GLFW_KEY_DELETE        = 261
+export const GLFW_KEY_RIGHT         = 262
+export const GLFW_KEY_LEFT          = 263
+export const GLFW_KEY_DOWN          = 264
+export const GLFW_KEY_UP            = 265
+export const GLFW_KEY_PAGE_UP       = 266
+export const GLFW_KEY_PAGE_DOWN     = 267
+export const GLFW_KEY_HOME          = 268
+export const GLFW_KEY_END           = 269
+export const GLFW_KEY_CAPS_LOCK     = 280
+export const GLFW_KEY_SCROLL_LOCK   = 281
+export const GLFW_KEY_NUM_LOCK      = 282
+export const GLFW_KEY_PRINT_SCREEN  = 283
+export const GLFW_KEY_PAUSE         = 284
+export const GLFW_KEY_F1            = 290
+export const GLFW_KEY_F2            = 291
+export const GLFW_KEY_F3            = 292
+export const GLFW_KEY_F4            = 293
+export const GLFW_KEY_F5            = 294
+export const GLFW_KEY_F6            = 295
+export const GLFW_KEY_F7            = 296
+export const GLFW_KEY_F8            = 297
+export const GLFW_KEY_F9            = 298
+export const GLFW_KEY_F10           = 299
+export const GLFW_KEY_F11           = 300
+export const GLFW_KEY_F12           = 301
+export const GLFW_KEY_F13           = 302
+export const GLFW_KEY_F14           = 303
+export const GLFW_KEY_F15           = 304
+export const GLFW_KEY_F16           = 305
+export const GLFW_KEY_F17           = 306
+export const GLFW_KEY_F18           = 307
+export const GLFW_KEY_F19           = 308
+export const GLFW_KEY_F20           = 309
+export const GLFW_KEY_F21           = 310
+export const GLFW_KEY_F22           = 311
+export const GLFW_KEY_F23           = 312
+export const GLFW_KEY_F24           = 313
+export const GLFW_KEY_F25           = 314
+export const GLFW_KEY_KP_0          = 320
+export const GLFW_KEY_KP_1          = 321
+export const GLFW_KEY_KP_2          = 322
+export const GLFW_KEY_KP_3          = 323
+export const GLFW_KEY_KP_4          = 324
+export const GLFW_KEY_KP_5          = 325
+export const GLFW_KEY_KP_6          = 326
+export const GLFW_KEY_KP_7          = 327
+export const GLFW_KEY_KP_8          = 328
+export const GLFW_KEY_KP_9          = 329
+export const GLFW_KEY_KP_DECIMAL    = 330
+export const GLFW_KEY_KP_DIVIDE     = 331
+export const GLFW_KEY_KP_MULTIPLY   = 332
+export const GLFW_KEY_KP_SUBTRACT   = 333
+export const GLFW_KEY_KP_ADD        = 334
+export const GLFW_KEY_KP_ENTER      = 335
+export const GLFW_KEY_KP_EQUAL      = 336
+export const GLFW_KEY_LEFT_SHIFT    = 340
+export const GLFW_KEY_LEFT_CONTROL  = 341
+export const GLFW_KEY_LEFT_ALT      = 342
+export const GLFW_KEY_LEFT_SUPER    = 343
+export const GLFW_KEY_RIGHT_SHIFT   = 344
+export const GLFW_KEY_RIGHT_CONTROL = 345
+export const GLFW_KEY_RIGHT_ALT     = 346
+export const GLFW_KEY_RIGHT_SUPER   = 347
+export const GLFW_KEY_MENU          = 348
+export const GLFW_KEY_LAST          = GLFW_KEY_MENU
 
 // Let's load that library.
 const { 
@@ -1266,69 +1266,4 @@ export {
   GLFW_VERSION_MAJOR,
   GLFW_VERSION_MINOR,
   GLFW_VERSION_REVISION,
-  GLFW_FOCUSED,
-  GLFW_ICONIFIED,
-  GLFW_RESIZABLE,
-  GLFW_VISIBLE,
-  GLFW_DECORATED,
-  GLFW_AUTO_ICONIFY,
-  GLFW_FLOATING,
-  GLFW_MAXIMIZED,
-  GLFW_CENTER_CURSOR,
-  GLFW_TRANSPARENT_FRAMEBUFFER,
-  GLFW_HOVERED,
-  GLFW_FOCUS_ON_SHOW,
-  GLFW_RED_BITS,
-  GLFW_GREEN_BITS,
-  GLFW_BLUE_BITS,
-  GLFW_ALPHA_BITS,
-  GLFW_DEPTH_BITS,
-  GLFW_STENCIL_BITS,
-  GLFW_ACCUM_RED_BITS,
-  GLFW_ACCUM_GREEN_BITS,
-  GLFW_ACCUM_BLUE_BITS,
-  GLFW_ACCUM_ALPHA_BITS,
-  GLFW_AUX_BUFFERS,
-  GLFW_STEREO,
-  GLFW_SAMPLES,
-  GLFW_SRGB_CAPABLE,
-  GLFW_REFRESH_RATE,
-  GLFW_DOUBLEBUFFER,
-  GLFW_CLIENT_API,
-  GLFW_CONTEXT_VERSION_MAJOR,
-  GLFW_CONTEXT_VERSION_MINOR,
-  GLFW_CONTEXT_REVISION,
-  GLFW_CONTEXT_ROBUSTNESS,
-  GLFW_OPENGL_FORWARD_COMPAT,
-  GLFW_OPENGL_DEBUG_CONTEXT,
-  GLFW_OPENGL_PROFILE,
-  GLFW_CONTEXT_RELEASE_BEHAVIOR,
-  GLFW_CONTEXT_NO_ERROR,
-  GLFW_CONTEXT_CREATION_API,
-  GLFW_SCALE_TO_MONITOR,
-  GLFW_COCOA_RETINA_FRAMEBUFFER,
-  GLFW_COCOA_FRAME_NAME,
-  GLFW_COCOA_GRAPHICS_SWITCHING,
-  GLFW_X11_CLASS_NAME,
-  GLFW_X11_INSTANCE_NAME,
-
-  //* Error codes
-  GLFW_NO_ERROR,
-  GLFW_NOT_INITIALIZED,
-  GLFW_NO_CURRENT_CONTEXT,
-  GLFW_INVALID_ENUM,
-  GLFW_INVALID_VALUE,
-  GLFW_OUT_OF_MEMORY,
-  GLFW_API_UNAVAILABLE,
-  GLFW_VERSION_UNAVAILABLE,
-  GLFW_PLATFORM_ERROR,
-  GLFW_FORMAT_UNAVAILABLE,
-  GLFW_NO_WINDOW_CONTEXT,
-
-  //* Whatever this is https://www.glfw.org/docs/latest/group__init.html
-  GLFW_TRUE,
-  GLFW_FALSE,
-  GLFW_JOYSTICK_HAT_BUTTONS,
-  GLFW_COCOA_CHDIR_RESOURCES,
-  GLFW_COCOA_MENUBAR,
 }
